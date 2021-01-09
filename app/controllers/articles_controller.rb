df64d31b0368cc244c9ad9226789515cddce43cd
Huy Do <hihuydo@gmail.com>
2021-01-10 00:04:17 +0100

    almost done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,39 @@
 class ArticlesController < ApplicationController
+  # before_action :find_article
+
   def index
-    @articles = Article.all
+    @articles = Article.all.order('id DESC')
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
